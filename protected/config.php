e417c8103767497c8576a2345902b764e50bdfb9
Aux Ciprián <60713159+cipoleon@users.noreply.github.com>
2020-12-08 13:29:17 +0100

    Update config.php

--- protected/config.php
+++ protected/config.php
@@ -7,9 +7,9 @@ define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
 define('USER_MANAGER', PROTECTED_DIR.'userManager.php');
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'b3ct5f');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'sql7.freemysqlhosting.net');
+define('DB_NAME', 'sql7380926');
+define('DB_USER', 'sql7380926');
+define('DB_PASS', 'd6TdPUFaYU');
 define('DB_CHARSET', 'utf8');
 ?>
